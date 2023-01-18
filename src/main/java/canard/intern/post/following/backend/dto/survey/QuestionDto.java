849819eff23cc694cd50d8cebe8ad0e065f6f37e
Vincent Crjlt-rd <vincentcr81@hotmail.fr>
2023-01-18 14:54:04 +0100

    restored

--- src/main/java/canard/intern/post/following/backend/dto/survey/QuestionDto.java
+++ src/main/java/canard/intern/post/following/backend/dto/survey/QuestionDto.java
@@ -1,4 +1,33 @@
 package canard.intern.post.following.backend.dto.survey;
 
+import canard.intern.post.following.backend.entity.Choice;
+import canard.intern.post.following.backend.enums.QuestionType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@ToString
+@Getter
+@Setter
 public class QuestionDto {
+
+
+    private Integer id ;
+
+    @NotBlank
+    @NotNull
+    private String title ;
+
+    @NotBlank
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private QuestionType questionType ;
+
+    private Set<Choice> choices = new HashSet<>();
 }
