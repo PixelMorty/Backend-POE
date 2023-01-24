9e676302b7fc41d13b9c0fff372cbfcd7445cf3d
Lola Haverlan <haverlan.lola@hotmail.fr>
2023-01-24 10:23:52 +0100

    Survey commit

--- src/main/java/canard/intern/post/following/backend/dto/survey/SurveyDto.java
+++ src/main/java/canard/intern/post/following/backend/dto/survey/SurveyDto.java
@@ -1,39 +1,15 @@
 package canard.intern.post.following.backend.dto.survey;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ff0f2076d4d60765475d1560acc163ecf7695afa
 import canard.intern.post.following.backend.entity.Question;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-<<<<<<< HEAD
-import javax.persistence.ManyToMany;
-=======
 import javax.persistence.*;
->>>>>>> ff0f2076d4d60765475d1560acc163ecf7695afa
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
-<<<<<<< HEAD
-
-@Getter  @Setter
-@ToString
-
-
-public class SurveyDto {
-    private  Integer id;
-
-    @NotNull
-    @NotBlank
-    private String title;
-
-
-
-=======
 @ToString
 @Getter
 @Setter
@@ -43,11 +19,10 @@ public class SurveyDto {
 
     private Integer id;
 
-@NotNull
-@NotBlank
+    @NotNull
+    @NotBlank
     private String title;
 
->>>>>>> ff0f2076d4d60765475d1560acc163ecf7695afa
     private Set<Question> questions = new HashSet<>();
 
 }
